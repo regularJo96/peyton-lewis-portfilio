41bbfa23f87eae7918e32d7d516a2d01f5b39f46
Josiah Anderson <riddlerforever@live.com>
2021-12-31 05:16:51 -0600

    added custom values to bootstrap; moved the arrows on the home page further from the edge

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,7 +14,7 @@ function Home(){
         <div className="container-fluid">
           <Welcome/>
           <a href="#charlie" className="menu light">
-          <span class="material-icons position-absolute bottom-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute bottom-10 start-50 translate-middle-x md-48">
             arrow_downward
           </span>
         </a>
@@ -23,7 +23,7 @@ function Home(){
 
       <div id="charlie" className="position-relative block container-fluid bg-snoopy text-center justify-content-center text-dark border border-dark">
         <a href="#top" className="menu dark">
-          <span class="material-icons position-absolute top-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute top-10 start-50 translate-middle-x md-48">
             arrow_upward
           </span>
         </a>
@@ -33,7 +33,7 @@ function Home(){
         <Work title="You're a Good Man Charlie Brown" time="2017-2018" role="Snoopy" route="/You're-a-Good-Man-Charlie-Brown" color="white"/>
         <Link to="/You're-a-Good-Man-Charlie-Brown">Gallery</Link>
         <a href="#shirley" className="menu dark">
-          <span class="material-icons position-absolute bottom-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute bottom-10 start-50 translate-middle-x md-48">
             arrow_downward
           </span>
         </a>
@@ -41,7 +41,7 @@ function Home(){
 
       <div id="shirley" className="position-relative block container-fluid bg-shirley text-center justify-content-center text-shirleyC">
         <a href="#charlie" className="menu shirley">
-          <span class="material-icons position-absolute top-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute top-10 start-50 translate-middle-x md-48">
             arrow_upward
           </span>
         </a>
@@ -51,7 +51,7 @@ function Home(){
         <Work title="Some of My Best Friends are Smiths" time="2017-2018" role="Shirley Robinson" route="/Some-of-My-Best-Friends-are-Smiths"/>
         <Link to="/Some-of-My-Best-Friends-are-Smiths">Gallery</Link>
         <a href="#wonka" className="menu shirley">
-          <span class="material-icons position-absolute bottom-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute bottom-10 start-50 translate-middle-x md-48">
             arrow_downward
           </span>
         </a>
@@ -59,7 +59,7 @@ function Home(){
 
       <div id="wonka" className="position-relative block container-fluid bg-purple text-center justify-content-center text-wonka">
         <a href="#shirley" className="menu wonka">
-          <span class="material-icons position-absolute top-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute top-10 start-50 translate-middle-x md-48">
             arrow_upward
           </span>
         </a>
@@ -69,7 +69,7 @@ function Home(){
         <Work title="Roald Dahl's Willy Wonka" time="2018-2019" role="Grandma Josephine" route="/Roald-Dahl's-Willy-Wonka"/>
         <Link to="/Roald-Dahl's-Willy-Wonka">Gallery</Link>
         <a href="#ethel" className="menu wonka">
-          <span class="material-icons position-absolute bottom-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute bottom-10 start-50 translate-middle-x md-48">
             arrow_downward
           </span>
         </a>
@@ -77,7 +77,7 @@ function Home(){
 
       <div id="ethel" className="position-relative block container-fluid bg-ethel text-center justify-content-center text-ethelC">
         <a href="#wonka" className="menu ethel">
-          <span class="material-icons position-absolute top-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute top-10 start-50 translate-middle-x md-48">
             arrow_upward
           </span>
         </a>
@@ -87,7 +87,7 @@ function Home(){
         <Work title="Curious Savage" time="2019-2020" role="Ethel P. Savage" route="/Curious-Savage"/>
         <Link to="/Curious-Savage">Gallery</Link>
         <a href="#music" className="menu ethel">
-          <span class="material-icons position-absolute bottom-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute bottom-10 start-50 translate-middle-x md-48">
             arrow_downward
           </span>
         </a>
@@ -95,7 +95,7 @@ function Home(){
      
       <div id="music" className="position-relative block container-fluid bg-green text-center justify-content-center text-light">
         <a href="#ethel" className="menu light">
-          <span class="material-icons position-absolute top-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute top-10 start-50 translate-middle-x md-48">
             arrow_upward
           </span>
         </a>
@@ -105,7 +105,7 @@ function Home(){
         <Work title="The Sound of Music" time="2019-2020" role="Student Director" route="/The-Sound-of-Music"/>
         <Link to="/The-Sound-of-Music">Gallery</Link>
         <a href="#anatomy" className="menu light">
-          <span class="material-icons position-absolute bottom-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute bottom-10 start-50 translate-middle-x md-48">
             arrow_downward
           </span>
         </a>
@@ -113,7 +113,7 @@ function Home(){
       
       <div id="anatomy" className="position-relative block container-fluid bg-dark text-center justify-content-center text-light">
         <a href="#music" className="menu light">
-          <span class="material-icons position-absolute top-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute top-10 start-50 translate-middle-x md-48">
             arrow_upward
           </span>
         </a>
@@ -123,7 +123,7 @@ function Home(){
         <Work title="Anatomy of Gray" time="2020-2021" role="Director" route="/Anatomy-of-Gray"/>
         <Link to="/Anatomy-of-Gray">Gallery</Link>
         <a href="#sylvia" className="menu light">
-          <span class="material-icons position-absolute bottom-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute bottom-10 start-50 translate-middle-x md-48">
             arrow_downward
           </span>
         </a>
@@ -131,7 +131,7 @@ function Home(){
       
       <div id="sylvia" className="position-relative block container-fluid bg-red text-center justify-content-center text-light">
         <a href="#anatomy" className="menu light">
-          <span class="material-icons position-absolute top-0 start-50 translate-middle-x md-48">
+          <span class="material-icons position-absolute top-10 start-50 translate-middle-x md-48">
             arrow_upward
           </span>
         </a>
