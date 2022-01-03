7f4a6622847d7a3e5bbe665985e3b0e3442bdd14
Josiah Anderson <riddlerforever@live.com>
2022-01-02 21:58:23 -0600

    Simplified the intro text

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -5,8 +5,8 @@ function Welcome(){
   const welcome = (
     <>
       <p className="display-1">HI, I'm Peyton.</p>
-      <p className="display-1">I enjoy doing plays.</p>
-      <p className="display-3">Scroll down or click the arrows to see my work.</p>
+      
+      <p className="display-3 ">Scroll down or click the arrows to see my productions.</p>
     </>
   )
 
