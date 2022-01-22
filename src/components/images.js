6191ca10a0a9caedebe51ecd1b4d16427f50e6ce
Josiah Anderson <riddlerforever@live.com>
2022-01-21 23:38:01 -0600

    re-organized the assets/images folder; added the 'don't be afraid of the dark' pictures

--- src/components/images.js
+++ src/components/images.js
@@ -1,27 +1,34 @@
-import Snoopy0 from '../assets/images/charlie-brown-1.jpg';
-import Snoopy1 from '../assets/images/charlie-brown-2.jpg';
+import Snoopy0 from '../assets/images/charlie-brown/charlie-brown-1.jpg';
+import Snoopy1 from '../assets/images/charlie-brown/charlie-brown-2.jpg';
 
-import Smiths1 from '../assets/images/smiths-1.jpg';
-import Smiths2 from '../assets/images/smiths-2.jpg';
-import Smiths3 from '../assets/images/smiths-3.jpg';
+import Smiths1 from '../assets/images/smiths/smiths-1.jpg';
+import Smiths2 from '../assets/images/smiths/smiths-2.jpg';
+import Smiths3 from '../assets/images/smiths/smiths-3.jpg';
 
-import Wonka1 from '../assets/images/wonka-1.jpg';
-import Wonka2 from '../assets/images/wonka-2.jpg';
-import Wonka3 from '../assets/images/wonka-3.jpg';
+import Wonka1 from '../assets/images/wonka/wonka-1.jpg';
+import Wonka2 from '../assets/images/wonka/wonka-2.jpg';
+import Wonka3 from '../assets/images/wonka/wonka-3.jpg';
 
-import Savage1 from '../assets/images/savage-1.jpg';
-import Savage2 from '../assets/images/savage-2.jpg';
-import Savage3 from '../assets/images/savage-3.jpg';
-import Savage4 from '../assets/images/savage-4.jpg';
-import Savage5 from '../assets/images/savage-5.jpg';
+import Savage1 from '../assets/images/savage/savage-1.jpg';
+import Savage2 from '../assets/images/savage/savage-2.jpg';
+import Savage3 from '../assets/images/savage/savage-3.jpg';
+import Savage4 from '../assets/images/savage/savage-4.jpg';
+import Savage5 from '../assets/images/savage/savage-5.jpg';
 
-import Music1 from '../assets/images/sound-of-music-1.jpg';
-import Music2 from '../assets/images/sound-of-music-2.jpg';
+import Music1 from '../assets/images/sound-of-music/sound-of-music-1.jpg';
+import Music2 from '../assets/images/sound-of-music/sound-of-music-2.jpg';
 
-import Anatomy1 from '../assets/images/anatomy-1.jpg';
-import Anatomy2 from '../assets/images/anatomy-2.jpg';
-import Anatomy3 from '../assets/images/anatomy-3.jpg';
-import Anatomy4 from '../assets/images/anatomy-4.jpg';
+import Anatomy1 from '../assets/images/anatomy/anatomy-1.jpg';
+import Anatomy2 from '../assets/images/anatomy/anatomy-2.jpg';
+import Anatomy3 from '../assets/images/anatomy/anatomy-3.jpg';
+import Anatomy4 from '../assets/images/anatomy/anatomy-4.jpg';
+
+import Dark1 from '../assets/images/dark/dark-1.jpg';
+import Dark2 from '../assets/images/dark/dark-2.jpg';
+import Dark3 from '../assets/images/dark/dark-3.jpg';
+import Dark4 from '../assets/images/dark/dark-4.jpg';
+import Dark5 from '../assets/images/dark/dark-5.jpg';
+import Dark6 from '../assets/images/dark/dark-6.jpg';
 
 export default [
   Snoopy0,
@@ -42,5 +49,11 @@ export default [
   Anatomy1, 
   Anatomy2, 
   Anatomy3, 
-  Anatomy4 
+  Anatomy4,
+  Dark1,
+  Dark2,
+  Dark3,
+  Dark4,
+  Dark5,
+  Dark6
 ];
